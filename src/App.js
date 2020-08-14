475396d38b968743335db45430fa787b52f84d3a
sarahmarie1976 <sholle7@gmail.com>
2020-08-13 21:08:19 -0500

    Removed service worker and changed header

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Better start learning this for Build week!!!! </h1>
       <h2>
         Deployed with{' '}
         <a
